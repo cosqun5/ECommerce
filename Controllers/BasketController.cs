0b86dd304adc5cdb9a82dd934159d05933386546
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-11-07 14:42:04 +0400

    update basketviewcomponent no reflesh

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -13,38 +13,57 @@ namespace Furn.Controllers
 		{
 			_context = context;
 		}
-		public IActionResult AddBasket(int id)
-		{
 
-			List<BasketVM> basket;
-			if (Request.Cookies[COOKIES_BASKET] != null)
-			{
-				basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies[COOKIES_BASKET]);
-			}
-			else
-			{
-				basket = new List<BasketVM> { };
-			}
-			BasketVM cookiesBasket = basket.Where(s => s.ProductId == id).FirstOrDefault();
-			if (cookiesBasket != null)
-			{
-				cookiesBasket.Count++;
-			}
-			else
-			{
 
-				BasketVM basketVM = new BasketVM()
-				{
-					ProductId = id,
-					Count = 1
-				};
-				basket.Add(basketVM);
-			}
+        public IActionResult GetBasket()
+        {
+            var model = new List<BasketItemVM>(); // Sepet verilerini yükleyen bir işlev çağırılabilir
 
-			HttpContext.Response.Cookies.Append(COOKIES_BASKET, JsonConvert.SerializeObject(basket));
-			return RedirectToAction("Index","Home");
-		}
-		public IActionResult RemoveBasket(int id)
+            return ViewComponent("Basket", model);
+        }
+        [HttpPost]
+        public IActionResult AddBasket(int id)
+        {
+            // Əlavə etmək istədiyiniz məhsulun id-sini alın
+            List<BasketVM> basket;
+
+            if (Request.Cookies[COOKIES_BASKET] != null)
+            {
+                basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies[COOKIES_BASKET]);
+
+            }
+            else
+            {
+                basket = new List<BasketVM>();
+            }
+
+            // Basket-da bu məhsulu axtar
+            BasketVM cookiesBasket = basket.Where(s => s.ProductId == id).FirstOrDefault();
+
+            if (cookiesBasket != null)
+            {
+                cookiesBasket.Count++;
+            }
+            else
+            {
+                BasketVM basketVM = new BasketVM()
+                {
+                    ProductId = id,
+                    Count = 1
+                };
+                basket.Add(basketVM);
+            }
+
+            // Yenidən cookies-i yazın
+            Response.Cookies.Append(COOKIES_BASKET, JsonConvert.SerializeObject(basket), new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddMinutes(30) // Expires'i istəyinizə görə tənzimləyin
+            });
+
+            // JSON cavabını qaytarın ki AJAX-a göndərə bilsin
+            return Json(new { success = true, message = "Məhsul baskete əlavə edildi" });
+        }
+        public IActionResult RemoveBasket(int id)
 		{
 			List<BasketVM> basket;
 			if (Request.Cookies[COOKIES_BASKET] != null)
